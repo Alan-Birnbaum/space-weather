deea97ea3327b4bbfbc4190fdbe7f826a02e735a
Alan-Birnbaum <abirn0802@yahoo.com>
2024-01-24 19:20:49 +0000

    New Updates and discussion page

---
+++ Discussion_Outlook/getSWPCdis.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -O discussion.txt -q --show-progress https://services.swpc.noaa.gov/text/discussion.txt
+
+wget  -O advisory-outlook.txt -q --show-progress https://services.swpc.noaa.gov/text/advisory-outlook.txt
